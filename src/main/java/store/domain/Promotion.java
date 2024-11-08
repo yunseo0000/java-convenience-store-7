3210a834ecfb32c1098b5a7364c92cdb454642f4
이윤서 <ddal0524@inu.ac.kr>
2024-11-09 00:31:29 +0900

    feat : 프로모션 정보를 관리하는 기능 구현

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,4 +1,35 @@
 package store.domain;
 
 public class Promotion {
+    private final String name;
+    private final String targetProduct;
+    private final int requiredQuantity;
+    private final int freeQuantity;
+
+    public Promotion(String name, String targetProduct, int requiredQuantity, int freeQuantity) {
+        this.name = name;
+        this.targetProduct = targetProduct;
+        this.requiredQuantity = requiredQuantity;
+        this.freeQuantity = freeQuantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTargetProduct() {
+        return targetProduct;
+    }
+
+    public int getRequiredQuantity() {
+        return requiredQuantity;
+    }
+
+    public int getFreeQuantity() {
+        return freeQuantity;
+    }
+
+    public boolean isApplicable(int quantity) {
+        return quantity >= requiredQuantity;
+    }
 }
